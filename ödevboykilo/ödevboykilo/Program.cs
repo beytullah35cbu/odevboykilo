5e647e3be147d7c89e066d7d30b6d5cf9ccbf7b6
beytullah <114831003+beytullah35cbu@users.noreply.github.com>
2023-11-16 22:31:42 +0300

    Update Program.cs

--- ödevboykilo/ödevboykilo/Program.cs
+++ ödevboykilo/ödevboykilo/Program.cs
@@ -1,58 +1,41 @@
-﻿using System;
+using System;
 
-namespace IdealKiloHesaplama
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Vücut Kitle İndeksi Hesaplama");
+
+        // Kullanıcıdan boyu al
+        Console.Write("Boyunuzu (metre cinsinden) girin: ");
+        double boy = Convert.ToDouble(Console.ReadLine());
+
+        // Kullanıcıdan kiloyu al
+        Console.Write("Kilonuzu girin: ");
+        double kilo = Convert.ToDouble(Console.ReadLine());
+
+        // Vücut kitle indeksini hesapla
+        double vucutKitleIndeksi = kilo / (boy * boy);
+
+        // Sonucu ekrana yazdır
+        Console.WriteLine("Vücut Kitle İndeksi: " + vucutKitleIndeksi);
+
+        // Vücut kitle indeksi yorumunu yap
+        if (vucutKitleIndeksi < 18.5)
         {
-            // Kullanıcıdan boy, kilo ve cinsiyet bilgilerini alın
-            Console.Write("Lütfen boyunuzu (cm cinsinden) girin: ");
-            double boyCm = double.Parse(Console.ReadLine());
-
-            Console.Write("Lütfen kilonuzu (kg cinsinden) girin: ");
-            double kilo = double.Parse(Console.ReadLine());
-
-            Console.Write("Lütfen cinsiyetinizi girin (Kadın için 'K', Erkek için 'E'): ");
-            char cinsiyet = char.ToUpper(Console.ReadKey().KeyChar);
-
-            // İnç cinsinden boyu hesaplayın (1 inç = 2.54 cm)
-            double boyInch = boyCm / 2.54;
-
-            // Cinsiyete göre ideal kiloyu hesaplayın
-            double idealKilo ;
-
-            if (cinsiyet == 'K')
-            {
-                idealKilo = 45.5 + 2.3 * (boyInch - 60);
-            }
-            else if (cinsiyet == 'E')
-            {
-                idealKilo = 50 + 2.3 * (boyInch - 60);
-            }
-            else
-            {
-                Console.WriteLine("\nGeçersiz cinsiyet girişi. Lütfen 'K' veya 'E' girin.");
-                return;
-            }
-
-            // Ideal kiloyu ve kullanıcının kilosunu karşılaştırın
-            Console.WriteLine($"\nIdeal kilonuz: {idealKilo:F2} kg");
-
-            if (kilo == idealKilo)
-            {
-                Console.WriteLine("Kilonuz ideal.");
-            }
-            else if (kilo < idealKilo)
-            {
-                Console.WriteLine("Kilonuz ideal kilonun altında.");
-            }
-            else
-            {
-                Console.WriteLine("Kilonuz ideal kilonun üstünde.");
-            }
-
-            Console.ReadLine();
+            Console.WriteLine("Zayıf");
+        }
+        else if (vucutKitleIndeksi >= 18.5 && vucutKitleIndeksi < 24.9)
+        {
+            Console.WriteLine("Normal");
+        }
+        else if (vucutKitleIndeksi >= 25 && vucutKitleIndeksi < 29.9)
+        {
+            Console.WriteLine("Fazla Kilolu");
+        }
+        else
+        {
+            Console.WriteLine("Obez");
         }
     }
 }
